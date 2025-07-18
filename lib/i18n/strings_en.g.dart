57ca1f58a156290f7e060bdd8a760654f177f04f
Aarron Lee <aarron-lee@users.noreply.github.com>
2025-07-17 20:47:34 -0400

    fix: Android custom directory, original fix from ComputerElite

--- lib/i18n/strings_en.g.dart
+++ lib/i18n/strings_en.g.dart
@@ -472,6 +472,10 @@ class TranslationsSettingsCustomDataDirEn {
 	// Translations
 	String get cancel => 'Cancel';
 	String get select => 'Select';
+	String get yes => 'Yes';
+	String get grantPermission => "Grant permission";
+	String get grantPermissionExplanation =>
+      "Saber needs permission to access the directory you chose in case you want to modify your notes via any other app (e. g. Syncthing). Do you want to grant this permission?";
 	String get mustBeEmpty => 'Selected folder must be empty';
 	String get mustBeDoneSyncing => 'Make sure syncing is complete before changing the folder';
 }
